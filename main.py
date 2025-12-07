3b8053c86b4c2e978b24a2c772765610dd1f0a08
SuzakuSar <loganjocsonsar@gmail.com>
2025-12-07 12:41:04 -0800

    Web version on localhost:7860 Picture uploading implemented

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from transformers import pipeline
 from PIL import Image
+import gradio as gr
+
+
 
 classifier = pipeline("image-classification", model="google/vit-base-patch16-224")
 
-results = classifier("Black.webp")
+def classify_image(image):
+    results = classifier(image)
+    output = ""
+    for result in results:
+        label = result['label']
+        confidence = result['score'] * 100
+        output += f"{label}: {confidence:.1f}% confident\n"
 
+    return output
 
-print("\n"*20)
-for result in results:
-    label = result['label']
-    confidence = result['score'] * 100
-    print(f"{label}: {confidence:.1f}% confident\n")
+# print("\n"*20)
+
+interface = gr.Interface(
+    fn=classify_image, # The funciton it calls
+    inputs=gr.Image(type="pil"), # What goes in as input
+    outputs="text", # What comes out as output
+    title="Image Classifier", # The title of the tab
+    description="Upload an image and it will get automatically classified", # The description
+    examples=[], # No examples as of now
+)
+
+if __name__ == "__main__":
+    interface.launch()
